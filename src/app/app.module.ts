2f2640678c8c9e414056b2de0e6e922758cf5725
Asen Santev <santev@gmail.com>
2020-09-15 00:13:28 +0300

    feat: added view and create components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
