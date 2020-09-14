01976da5643b052aec7d5be4c1e439e9f096a9df
Asen Santev <santev@gmail.com>
2020-09-15 00:18:46 +0300

    feat: added view cards

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,14 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimalService} from './../shared/animal.service'
 
 @Component({
-  selector: 'app-view',
-  templateUrl: './view.component.html',
-  styleUrls: ['./view.component.css']
+    selector: 'app-view',
+    templateUrl: './view.component.html',
+    styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
-  constructor() { }
+    myAnimals: any = [
+        {
+            name: 'dog',
+            sound: 'woof'
+        },
+        {
+            name: 'cat',
+            sound: 'meow'
+        },
+        {
+            name: 'pig',
+            sound: 'gruh'
+        },
+        {
+            name: 'cow',
+            sound: 'moo'
+        },
+        {
+            name: 'duck',
+            sound: 'kvak'
+        }
+    ];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private animalService: AnimalService
+    ) {
+    }
+
+    ngOnInit(): void {
+        if (this.animalService.customAnimals) {
+            this.myAnimals = this.myAnimals.concat(this.animalService.customAnimals);
+        }
+    }
 
 }
