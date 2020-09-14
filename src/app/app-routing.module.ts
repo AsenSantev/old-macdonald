aa805052795de4b174e53524f90c0fc4df0216fc
Asen Santev <santev@gmail.com>
2020-09-15 00:11:12 +0300

    feat: added routes - view, create, default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-const routes: Routes = [];
+import { ViewComponent } from "./view/view.component";
+import { CreateComponent } from "./create/create.component";
+
+const routes:Routes = [
+    {path: 'view', component: ViewComponent},
+    {path: 'create', component: CreateComponent},
+    {path: '', redirectTo: '/view', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes),
+        FormsModule,
+        ReactiveFormsModule,
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
